c432f167072e8c71c6fccf78581706dd8d5091e9
Vera Ferreira <verammferreira@gmail.com>
2023-06-27 14:17:20 +0100

    add navbar and header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <>
+        <h1>vera ferreira</h1>
+        <h2 className="header--h2">i build things on the web</h2>
+        </>
+
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <>
+            <ul className="nav--menu">
+                <li>about</li>
+                <li>experience</li>
+                <li>portfolio</li>
+                <li>contact</li>
+                <li>resume</li>
+            </ul>
+        </>
+    )
+}
