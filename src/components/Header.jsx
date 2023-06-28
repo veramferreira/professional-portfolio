7c0c1c774f6c463b7e74f5f4dfaa4f9aab5ffd0b
Vera Ferreira <verammferreira@gmail.com>
2023-06-28 16:02:06 +0100

    add more info in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-
-
 export default function Header() {
     return (
-        <>
-        <h1>vera ferreira</h1>
-        <h2 className="header--h2">i build things on the web</h2>
-        </>
-
-    )
-}
+      <div>
+        <section className="header">
+          <h1>vera ferreira</h1>
+          <h2 className="header--h2">i build things on the web</h2>
+        </section>
+        <section className="header--p">
+          <p>👩‍⚕️ nurse-turned-software developer</p>
+          <p>🎨 passionate about design and user experience</p>
+          <p>📺 entertainment aficionada</p>
+          <p>🇵🇹🇬🇧 portuguese living in the UK</p>
+        </section>
+      </div>
+    );
+  }
+  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 export default function Navbar() {
     return (
-        <>
+        <section className="nav">
             <ul className="nav--menu">
-                <li>about</li>
-                <li>experience</li>
-                <li>portfolio</li>
-                <li>contact</li>
-                <li>resume</li>
+                <li><a href="#about" className="nav--a"> about</a></li>
+                <li><a href="#experience" className="nav--a">experience</a></li>
+                <li><a href="#portfolio" className="nav--a">portfolio</a></li>
+                <li><a href="#contact" className="nav--a">contact</a></li>
+                <li><a href="#resume" className="nav--a">resume</a></li>
             </ul>
-        </>
+        </section>
     )
 }
