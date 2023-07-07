62914485a301ebe6b3b6c788b304e51a8008b889
Vera Ferreira <verammferreira@gmail.com>
2023-07-07 13:24:47 +0100

    add favicon and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,13 @@ export default function Navbar() {
     return (
         <section className='p-10'>
              <ul className="list-none flex justify-between items-center gap-5">
-        <li><TbSunFilled className=' text-bright-yellow  hover:text-bright-yellow/70 cursor-pointer transition-all'/></li>
+        <li>
+          <a href="#header">
+
+          <img src="logo2.png" alt="logo" className=' w-8'/>
+          </a>
+          {/* <TbSunFilled className=' text-bright-yellow  hover:text-bright-yellow/70 cursor-pointer transition-all'/> */}
+          </li>
         <div className="flex gap-5 font-mono text-bright-yellow ">
           <li className=' hover:text-bright-yellow/70'><a href="#about">about</a></li>
           <li className=' hover:text-bright-yellow/70'><a href="#experience">experience</a></li>
