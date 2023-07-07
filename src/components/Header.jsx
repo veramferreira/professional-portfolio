354a0686d64cb104d56a4a2199ee48c12a1c6273
Vera Ferreira <verammferreira@gmail.com>
2023-07-07 12:56:59 +0100

    fix button on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,34 @@
 export default function Header() {
-    return (
-      <div className=" pl-20" id="header">
-        <section className="px-10 pt-40">
-          <h1 className=" font-poppins font-extrabold text-7xl text-bright-pink" id="header--h1">vera ferreira</h1>
-          <h2 className=" font-poppins text-4xl tracking-wide text-bright-pink/80" id="header--h2">i build things on the web</h2>
-        </section>
-        <section className="px-10 pt-5 font-mono text-bright-pink/80 text-lg">
-          <p>👩‍⚕️ nurse-turned-software developer</p>
-          <p>🎨 passionate about design and user experience</p>
-          <p>📺 entertainment aficionada</p>
-          <p>🇵🇹 portuguese living in the UK 🇬🇧</p>
-        </section>
-        <section className="px-10 pt-10 pb-40 ">
-
-            <a href="#contact" className=" font-bold font-mono text-xs  text-bright-yellow transition-all ease-in duration-300 hover:bg-bright-yellow hover:text-deep-blue px-5 py-2 border rounded-lg hover:scale-105">let's work together</a>
-        </section>
-      </div>
-    );
-  }
-  
+  return (
+    <div className=" pl-20" id="header">
+      <section className="px-10 pt-40">
+        <h1
+          className=" font-poppins font-extrabold text-7xl text-bright-pink"
+          id="header--h1"
+        >
+          vera ferreira
+        </h1>
+        <h2
+          className=" font-poppins text-4xl tracking-wide text-bright-pink/80"
+          id="header--h2"
+        >
+          i build things on the web
+        </h2>
+      </section>
+      <section className="px-10 pt-5 font-mono text-bright-pink/80 text-lg">
+        <p>👩‍⚕️ nurse-turned-software developer</p>
+        <p>🎨 passionate about design and user experience</p>
+        <p>📺 entertainment aficionada</p>
+        <p>🇵🇹 portuguese living in the UK 🇬🇧</p>
+      </section>
+      <section className="px-10 pt-10 pb-40 ">
+        <a
+          href="#contact"
+          className=" font-bold font-mono text-sm  text-bright-yellow transition-all ease-in duration-300 hover:bg-bright-yellow hover:text-deep-blue px-5 py-2 border rounded-lg hover:scale-105"
+        >
+          let's work together
+        </a>
+      </section>
+    </div>
+  );
+}
