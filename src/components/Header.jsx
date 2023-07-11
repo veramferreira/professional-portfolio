d55edec0c1980ad2ab5a98104bd8cab3326cb078
Vera Ferreira <verammferreira@gmail.com>
2023-07-11 09:42:06 +0100

    fix atitle animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,37 @@
+import { useEffect, useRef } from "react";
 import SplitType from "split-type";
 import { gsap } from "gsap";
 
 export default function Header() {
-  const text = new SplitType("#header--h1");
+  const headerRef = useRef(null);
 
-  const characters = document.getElementsByClassName("char");
+  useEffect(() => {
+    const text = new SplitType("#header--h1");
+    const characters = headerRef.current.querySelectorAll(".char");
 
-  for (let i = 0; i < characters.length; i++) {
-    characters[i].classList.add("translate-y-full");
-  }
+    for (let i = 0; i < characters.length; i++) {
+      characters[i].classList.add("translate-y-full");
+    }
 
-  gsap.to(".char", {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.02,
-    duration: 0.5,
-  });
+    gsap.to(characters, {
+      y: 0,
+      stagger: 0.05,
+      delay: 0.02,
+      duration: 0.5,
+    });
+  }, []);
 
   return (
-    <div className=" pl-20" id="header">
-      <section className="px-10 pt-40">
+    <div className="pl-20" id="header">
+      <section className="px-10 pt-40 title" ref={headerRef}>
         <h1
-          className=" font-poppins font-extrabold text-7xl text-bright-pink"
+          className="font-poppins font-extrabold text-7xl text-bright-pink"
           id="header--h1"
           style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}
         >
           vera ferreira
         </h1>
-        <h2
-          className=" font-poppins text-4xl tracking-wide text-bright-pink/80"
-          id="header--h2"
-        >
+        <h2 className="font-poppins text-4xl tracking-wide text-bright-pink/80" id="header--h2">
           i build things on the web
         </h2>
       </section>
@@ -40,10 +41,10 @@ export default function Header() {
         <p>📺 entertainment aficionada</p>
         <p>🇵🇹 portuguese living in the UK 🇬🇧</p>
       </section>
-      <section className="px-10 pt-10 pb-40 ">
+      <section className="px-10 pt-10 pb-40">
         <a
           href="#contact"
-          className=" font-bold font-mono text-sm  text-bright-yellow transition-all ease-in duration-300 hover:bg-bright-yellow hover:text-deep-blue px-5 py-2 border rounded-lg hover:scale-105"
+          className="font-bold font-mono text-sm text-bright-yellow transition-all ease-in duration-300 hover:bg-bright-yellow hover:text-deep-blue px-5 py-2 border rounded-lg hover:scale-105"
         >
           let's work together
         </a>
